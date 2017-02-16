83ec5605aeb3425a34df3fca665fb4d473253365
Michael Hennerich <michael.hennerich@analog.com>
2017-02-16 13:53:54 +0100

    board/pluto/ifupdown: Write IP address to file
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- board/pluto/ifupdown.sh
+++ board/pluto/ifupdown.sh
@@ -3,8 +3,10 @@
 case "${ACTION}" in
 add|"")
 	ifup ${MDEV}
+	echo $(ip -f inet -o addr show ${MDEV}|cut -d\  -f 7 | cut -d/ -f 1) > /opt/ipaddr-${MDEV}
 	;;
 remove)
+	echo $(ip -f inet -o addr show ${MDEV}|cut -d\  -f 7 | cut -d/ -f 1) > /opt/ipaddr-${MDEV}
 	ifdown ${MDEV}
 	;;
 esac
