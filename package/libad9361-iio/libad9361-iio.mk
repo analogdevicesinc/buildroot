2e4817b2c0b18796ac319b80d3919664fb7d7c8a
Michael Hennerich <michael.hennerich@analog.com>
2019-01-14 12:52:18 +0100

    package: libad9361-iio: Bump to Version 0.2 - switch to release mechanism
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- package/libad9361-iio/libad9361-iio.mk
+++ package/libad9361-iio/libad9361-iio.mk
@@ -3,9 +3,8 @@
 # libad9361-iio
 #
 ################################################################################
-LIBAD9361_IIO_VERSION = b98b1cd2280d73ced04cb4cf9482b2d2d91e31a2
-LIBAD9361_IIO_SITE = https://github.com/analogdevicesinc/libad9361-iio.git
-LIBAD9361_IIO_SITE_METHOD = git
+LIBAD9361_IIO_VERSION = 0.2
+LIBAD9361_IIO_SITE = $(call github,analogdevicesinc,libad9361-iio,v$(LIBAD9361_IIO_VERSION))
 
 LIBAD9361_IIO_INSTALL_STAGING = YES
 LIBAD9361_IIO_LICENSE = LGPL-2.1+
