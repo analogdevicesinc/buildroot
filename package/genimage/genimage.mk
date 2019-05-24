360bd8422fa57ee9526adfe913bdc660bf27ae8e
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-05-24 11:41:23 +0300

    Revert "host-genimage: add host-mtools to dependency list"
    This reverts commit 980d89274675d2dfffd291ef41de313bc50e023d.
    
    Upstream refused this proposal.
    Remove it, and use Kconfig symbols to select this.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- package/genimage/genimage.mk
+++ package/genimage/genimage.mk
@@ -7,7 +7,7 @@
 GENIMAGE_VERSION = 10
 GENIMAGE_SOURCE = genimage-$(GENIMAGE_VERSION).tar.xz
 GENIMAGE_SITE = https://github.com/pengutronix/genimage/releases/download/v$(GENIMAGE_VERSION)
-HOST_GENIMAGE_DEPENDENCIES = host-pkgconf host-libconfuse host-mtools
+HOST_GENIMAGE_DEPENDENCIES = host-pkgconf host-libconfuse
 GENIMAGE_LICENSE = GPL-2.0
 GENIMAGE_LICENSE_FILES = COPYING
 
