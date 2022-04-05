ffef98e1fbdb63ef228b9bef774476b215a71e35
Michael Hennerich <michael.hennerich@analog.com>
2022-04-05 12:04:44 +0200

    package/libm2k: Bump to version 0.6.0 and avoid github dl issue
    For some reason if there is a matching branch name the download errors:
    
    HTTP request sent, awaiting response... 300 Multiple Choices
    Length: 105 [text/plain]
    
    and the downloaded files just contains:
    
    'the given path has multiple possibilities:
    #<Git::Ref:0x00007f3c4ae65ba8>, #<Git::Ref:0x00007f3c4ae66f08>'
    
    We avoid this by appending 'refs/tags'
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- package/libm2k/libm2k.mk
+++ package/libm2k/libm2k.mk
@@ -3,8 +3,8 @@
 # libm2k
 #
 ################################################################################
-LIBM2K_VERSION = 0.5.0
-LIBM2K_SITE = $(call github,analogdevicesinc,libm2k,v$(LIBM2K_VERSION))
+LIBM2K_VERSION = 0.6.0
+LIBM2K_SITE = https://github.com/analogdevicesinc/libm2k/archive/refs/tags/v$(LIBM2K_VERSION)
 
 LIBM2K_INSTALL_STAGING = YES
 LIBM2K_LICENSE = LGPL-2.1
