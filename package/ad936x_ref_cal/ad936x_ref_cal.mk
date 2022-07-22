7d8baca3dbbb649545111f16a5c6f84cd2779610
Benjamin Menküc <benjamin@menkuec.de>
2022-07-22 22:01:20 +0200

    fix ad936x_ref_cal hash

--- package/ad936x_ref_cal/ad936x_ref_cal.mk
+++ package/ad936x_ref_cal/ad936x_ref_cal.mk
@@ -6,8 +6,7 @@
 
 
 AD936X_REF_CAL_VERSION = 01747db5cd60ff64115a73ac1f3bb97911f5c58e
-AD936X_REF_CAL_SITE = https://github.com/analogdevicesinc/plutosdr_scripts.git
-AD936X_REF_CAL_SITE_METHOD = git
+AD936X_REF_CAL_SITE = $(call github,analogdevicesinc,plutosdr_scripts,$(AD936X_REF_CAL_VERSION))
 AD936X_REF_CAL_LICENSE = GPLv2
 AD936X_REF_CAL_LICENSE_FILES = LICENSE
 AD936X_REF_CAL_DEPENDENCIES = libiio fftw-double
