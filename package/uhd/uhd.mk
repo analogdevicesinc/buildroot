5c6b5abc0ed8600803485364f87ca00a4c31d756
info@revskills.de <info@revskills.de>
2023-02-23 19:24:39 +0100

    Add uhd 4.4

--- package/uhd/uhd.mk
+++ package/uhd/uhd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UHD_VERSION = 3.15.0.0
+UHD_VERSION = 4.4.0.0
 UHD_SITE = $(call github,EttusResearch,uhd,v$(UHD_VERSION))
 UHD_LICENSE = GPL-3.0+
 UHD_LICENSE_FILES = LICENSE.md host/LICENSE
