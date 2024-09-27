df124a0209d5bf97c68a5d7621527733a76b37b5
Michael Hennerich <michael.hennerich@analog.com>
2024-09-27 12:55:37 +0200

    package/jesd204b_status: Update to v0.1
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- package/jesd204b_status/jesd204b_status.mk
+++ package/jesd204b_status/jesd204b_status.mk
@@ -5,11 +5,15 @@
 ################################################################################
 
 
-JESD204B_STATUS_VERSION = 7cf1e3352214c70ce36dcac71d2bab3cb12dcf4c
-JESD204B_STATUS_SITE = https://github.com/analogdevicesinc/jesd-eye-scan-gtk.git
-JESD204B_STATUS_SITE_METHOD = git
+#JESD204B_STATUS_VERSION = 90cad36a2c09e0fbea9763aca4ae0f72f1677bbc
+#JESD204B_STATUS_SITE = https://github.com/analogdevicesinc/jesd-eye-scan-gtk.git
+#JESD204B_STATUS_SITE_METHOD = git
+
+JESD204B_STATUS_VERSION = 0.1
+JESD204B_STATUS_SITE = $(call github,analogdevicesinc,jesd-eye-scan-gtk,v$(JESD204B_STATUS_VERSION))
+
 JESD204B_STATUS_LICENSE = BSD
-JESD204B_STATUS_LICENSE_FILES = LICENSE
+JESD204B_STATUS_LICENSE_FILES = LICENSE.txt
 JESD204B_STATUS_DEPENDENCIES = ncurses
 
 define JESD204B_STATUS_BUILD_CMDS
