e5d751649c08da70a0227194425fbd7660bea908
Michael Hennerich <michael.hennerich@analog.com>
2025-02-26 17:07:43 +0100

    package/fru-tools: Update to the latest v0.8.1.7 release
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- package/fru-tools/fru-tools.mk
+++ package/fru-tools/fru-tools.mk
@@ -3,9 +3,8 @@
 # fru-tools
 #
 ################################################################################
-FRU_TOOLS_VERSION = 534c30f1ae9f10825c04f8f6522336e01a75afd7
-FRU_TOOLS_SITE = https://github.com/analogdevicesinc/fru_tools.git
-FRU_TOOLS_SITE_METHOD = git
+FRU_TOOLS_VERSION = 0.8.1.7
+FRU_TOOLS_SITE = $(call github,analogdevicesinc,fru_tools,v$(FRU_TOOLS_VERSION))
 FRU_TOOLS_INSTALL_STAGING = YES
 FRU_TOOLS_LICENSE = GPL-2.0
 FRU_TOOLS_LICENSE_FILES = license.txt
