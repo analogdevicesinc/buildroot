analogdevicesinc/buildroot

commit 128c26f2875dd60ed5fafe25f63b3da11bb79f60

boot/uboot: bump to version 2025.10
Release announcement:
https://lists.denx.de/pipermail/u-boot/2025-October/599798.html

Changelog:
https://github.com/u-boot/u-boot/compare/v2025.07...v2025.10

With upstream commit a239b0b0b6db ("Add symlink from gpl-2.0.txt to
a COPYING file"), COPYING is now a symlink to Licenses/gpl-2.0.txt,
causing the install command in UBOOT_COPY_OLD_LICENSE_FILE to fail
because the source and destination refer to the same file. To avoid
this, copy the license file only if Licenses/gpl-2.0.txt does not
already exist.

Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>
Signed-off-by: Julien Olivain <ju.o@free.fr>

boot/uboot/Config.in
@@ -41,7 +41,7 @@ choice
 	  Select the specific U-Boot version you want to use
 
 config BR2_TARGET_UBOOT_LATEST_VERSION
-	bool "2025.07"
+	bool "2025.10"
 
 config BR2_TARGET_UBOOT_CUSTOM_VERSION
 	bool "Custom version"
@@ -85,7 +85,7 @@ endif
 
 config BR2_TARGET_UBOOT_VERSION
 	string
-	default "2025.07"	if BR2_TARGET_UBOOT_LATEST_VERSION
+	default "2025.10"	if BR2_TARGET_UBOOT_LATEST_VERSION
 	default BR2_TARGET_UBOOT_CUSTOM_VERSION_VALUE \
 		if BR2_TARGET_UBOOT_CUSTOM_VERSION
 	default "custom"	if BR2_TARGET_UBOOT_CUSTOM_TARBALL
@@ -340,7 +340,7 @@ config BR2_TARGET_UBOOT_USE_BINMAN
 	help
 	  Use binman tool for generation and signing of boot images.
 
-	  https://docs.u-boot.org/en/v2025.07/develop/package/binman.html
+	  https://docs.u-boot.org/en/v2025.10/develop/package/binman.html
 
 menu "U-Boot binary format"
 

boot/uboot/uboot.mk
@@ -307,7 +307,7 @@ endif
 # prior to u-boot 2013.10 the license info was in COPYING. Copy it so
 # legal-info finds it
 define UBOOT_COPY_OLD_LICENSE_FILE
-	if [ -f $(@D)/COPYING ]; then \
+	if [ -f $(@D)/COPYING ] && [ ! -f $(@D)/Licenses/gpl-2.0.txt ]; then \
 		$(INSTALL) -m 0644 -D $(@D)/COPYING $(@D)/Licenses/gpl-2.0.txt; \
 	fi
 endef

boot/uboot/uboot.hash
@@ -1,3 +1,3 @@
 # Locally computed:
-sha256  0f933f6c5a426895bf306e93e6ac53c60870e4b54cda56d95211bec99e63bec7  u-boot-2025.07.tar.bz2
+sha256  b4f032848e56cc8f213ad59f9132c084dbbb632bc29176d024e58220e0efdf4a  u-boot-2025.10.tar.bz2
 sha256  8177f97513213526df2cf6184d8ff986c675afb514d4e68a404010521b880643  Licenses/gpl-2.0.txt

commit 2458e1777fbabb0d95ebe9f5322c19d1da607272

boot/uboot: use new default env Kconfig options
Upstream U-Boot commit fb5235239aad ("env: Rename DEFAULT_ENV_FILE to
ENV_DEFAULT_ENV_TEXT_FILE") renamed the Kconfig symbols used to define
a default environment file. CONFIG_USE_DEFAULT_ENV_FILE and
CONFIG_DEFAULT_ENV_FILE (for U-Boot < v2025.10) were renamed to
CONFIG_ENV_USE_DEFAULT_ENV_TEXT_FILE and
CONFIG_ENV_DEFAULT_ENV_TEXT_FILE (for U-Boot >= v2025.10).

Fixes: 128c26f2875d ("boot/uboot: bump to version 2025.10")
Signed-off-by: Ozan Durgut <ozan.durgut@analog.com>

boot/uboot/Config.in
@@ -164,8 +164,10 @@ config BR2_TARGET_UBOOT_DEFAULT_ENV_FILE
 	    with lines containing key=value pairs. Blank lines and
 	    lines beginning with '#' are ignored.
 
-	  Based on the USE_DEFAULT_ENV_FILE and DEFAULT_ENV_FILE U-Boot
-	  configuration variables.
+	  Based on the following U-Boot configuration variables:
+	  USE_DEFAULT_ENV_FILE and DEFAULT_ENV_FILE, or
+	  ENV_USE_DEFAULT_ENV_TEXT_FILE and ENV_DEFAULT_ENV_TEXT_FILE
+	  for U-Boot >= v2025.10.
 
 	  For more info see:
 	  https://docs.u-boot.org/en/latest/usage/environment.html#external-environment-file

boot/uboot/uboot.mk
@@ -414,8 +414,13 @@ UBOOT_KCONFIG_OPTS = $(UBOOT_MAKE_OPTS) HOSTCC="$(HOSTCC_NOCCACHE)" HOSTLDFLAGS=
 ifeq ($(BR2_TARGET_UBOOT_DEFAULT_ENV_FILE_ENABLED),y)
 UBOOT_DEFAULT_ENV_FILE = $(call qstrip,$(BR2_TARGET_UBOOT_DEFAULT_ENV_FILE))
 define UBOOT_KCONFIG_DEFAULT_ENV_FILE
+  ifeq ($(call uboot-version-ge,2025.10),y)
+	$(call KCONFIG_SET_OPT,CONFIG_ENV_USE_DEFAULT_ENV_TEXT_FILE,y)
+	$(call KCONFIG_SET_OPT,CONFIG_ENV_DEFAULT_ENV_TEXT_FILE,"$(shell readlink -f $(UBOOT_DEFAULT_ENV_FILE))")
+  else
 	$(call KCONFIG_SET_OPT,CONFIG_USE_DEFAULT_ENV_FILE,y)
 	$(call KCONFIG_SET_OPT,CONFIG_DEFAULT_ENV_FILE,"$(shell readlink -f $(UBOOT_DEFAULT_ENV_FILE))")
+  endif
 endef
 endif
 endif # BR2_TARGET_UBOOT_BUILD_SYSTEM_LEGACY