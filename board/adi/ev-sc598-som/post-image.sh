0a7f22950fd596a881a5b5557735e2c95f8a08fb
Philip Molloy <philip.molloy@analog.com>
2026-08-19 18:39:26 +0200

    board/adi/ev-sc598-som: use ext4 for eMMC boot partition
    ext4 provides many benefits over FAT and aligns with upstream U-Boot
    environment configuration.
    
    Signed-off-by: Philip Molloy <philip.molloy@analog.com>

--- board/adi/ev-sc598-som/post-image.sh
+++ board/adi/ev-sc598-som/post-image.sh
@@ -16,5 +16,11 @@ sed "s/@DEFAULT_DTB@/$default_dtb/" "$BOARD_DIR/kernel.its" \
   > "$BINARIES_DIR/kernel.its"
 (cd "$BINARIES_DIR" && "$HOST_DIR/bin/mkimage" -f kernel.its kernel.itb)
 
+# Assemble boot partition contents
+install -d "$BINARIES_DIR/boot"
+for f in Image sc598-htol.dtb sc598-som-ezkit.dtb sc598-som-ezlite.dtb; do
+  install -m 0644 "$BINARIES_DIR/$f" "$BINARIES_DIR/boot/"
+done
+
 # GDB script to load the two U-Boot stages over JTAG
 install -m 0644 "$BOARD_DIR/../u-boot.gdb" "$BINARIES_DIR/u-boot.gdb"
