analogdevicesinc/buildroot

commit 643b280d942b822ff67bc3f9fcf3c0a2fc9f4bec

configs/adi: Compress eMMC image
Migrate this logic from br2_external to consolidate to a single
post-image script. Also order the post-image script after genimage so it
runs after all images have been created.

Signed-off-by: Philip Molloy <philip@philipmolloy.com>

board/adi/ev-sc598-som/post-image.sh
@@ -19,3 +19,6 @@ done
 
 # GDB script to load the two U-Boot stages over JTAG
 install -m 0644 "$BOARD_DIR/../u-boot.gdb" "$BINARIES_DIR/u-boot.gdb"
+
+# TODO(KT): Workaround for https://github.com/analogdevicesinc/linux/issues/3400
+gzip -f -k "${BINARIES_DIR}/emmc.img"

board/adi/ev-sc846-som/post-image.sh
@@ -19,3 +19,6 @@ done
 
 # GDB script to load the two U-Boot stages over JTAG
 install -m 0644 "$BOARD_DIR/../u-boot.gdb" "$BINARIES_DIR/u-boot.gdb"
+
+# TODO(KT): Workaround for https://github.com/analogdevicesinc/linux/issues/3400
+gzip -f -k "${BINARIES_DIR}/emmc.img"

configs/adi_sc598_ezkit_defconfig
@@ -6,7 +6,7 @@ BR2_GLOBAL_PATCH_DIR="board/adi/patches"
 BR2_DOWNLOAD_FORCE_CHECK_HASHES=y
 BR2_TARGET_GENERIC_HOSTNAME="sc598-ezkit"
 BR2_TARGET_GENERIC_ISSUE="Welcome to the ADI SC598 EZ-KIT"
-BR2_ROOTFS_POST_IMAGE_SCRIPT="board/adi/ev-sc598-som/post-image.sh support/scripts/genimage.sh"
+BR2_ROOTFS_POST_IMAGE_SCRIPT="support/scripts/genimage.sh board/adi/ev-sc598-som/post-image.sh"
 BR2_ROOTFS_POST_IMAGE_SCRIPT_ARGS="-c board/adi/ev-sc598-som/genimage.cfg"
 BR2_LINUX_KERNEL=y
 BR2_LINUX_KERNEL_CUSTOM_GIT=y

configs/adi_sc598_ezlite_defconfig
@@ -6,7 +6,7 @@ BR2_GLOBAL_PATCH_DIR="board/adi/patches"
 BR2_DOWNLOAD_FORCE_CHECK_HASHES=y
 BR2_TARGET_GENERIC_HOSTNAME="sc598-ezlite"
 BR2_TARGET_GENERIC_ISSUE="Welcome to the ADI SC598 EZ-LITE"
-BR2_ROOTFS_POST_IMAGE_SCRIPT="board/adi/ev-sc598-som/post-image.sh support/scripts/genimage.sh"
+BR2_ROOTFS_POST_IMAGE_SCRIPT="support/scripts/genimage.sh board/adi/ev-sc598-som/post-image.sh"
 BR2_ROOTFS_POST_IMAGE_SCRIPT_ARGS="-c board/adi/ev-sc598-som/genimage.cfg"
 BR2_LINUX_KERNEL=y
 BR2_LINUX_KERNEL_CUSTOM_GIT=y

configs/adi_sc846_ezkit_defconfig
@@ -9,7 +9,7 @@ BR2_DOWNLOAD_FORCE_CHECK_HASHES=y
 BR2_PER_PACKAGE_DIRECTORIES=y
 BR2_TARGET_GENERIC_HOSTNAME="sc846-ezkit"
 BR2_TARGET_GENERIC_ISSUE="Welcome to the ADI SC846 EZ-KIT"
-BR2_ROOTFS_POST_IMAGE_SCRIPT="board/adi/ev-sc846-som/post-image.sh support/scripts/genimage.sh"
+BR2_ROOTFS_POST_IMAGE_SCRIPT="support/scripts/genimage.sh board/adi/ev-sc846-som/post-image.sh"
 BR2_ROOTFS_POST_IMAGE_SCRIPT_ARGS="-c board/adi/ev-sc846-som/genimage.cfg"
 BR2_LINUX_KERNEL=y
 BR2_LINUX_KERNEL_CUSTOM_GIT=y

commit 57de38f3b1d2366644e907e21849870a1dd71147

configs/adi: Fix post image script order
genimage.cfg requires a FIT image be generated in post-image.sh.
Therefore do the eMMC compression using genimage.

Fixes: 643b280d94 ("configs/adi: Compress eMMC image")
Signed-off-by: Philip Molloy <philip@philipmolloy.com>

board/adi/ev-sc598-som/genimage.cfg
@@ -43,6 +43,9 @@ image emmc.img {
 		image = "rootfs.ext4"
 		size = 7135M
 	}
+
+	# TODO(KT): Workaround for https://github.com/analogdevicesinc/linux/issues/3400
+	exec-post = 'gzip -f -k "$IMAGEOUTFILE"'
 }
 
 # ISSI Quad 128 Megabyte SPI flash

board/adi/ev-sc598-som/post-image.sh
@@ -19,6 +19,3 @@ done
 
 # GDB script to load the two U-Boot stages over JTAG
 install -m 0644 "$BOARD_DIR/../u-boot.gdb" "$BINARIES_DIR/u-boot.gdb"
-
-# TODO(KT): Workaround for https://github.com/analogdevicesinc/linux/issues/3400
-gzip -f -k "${BINARIES_DIR}/emmc.img"

board/adi/ev-sc846-som/genimage.cfg
@@ -43,6 +43,9 @@ image emmc.img {
 		image = "rootfs.ext4"
 		size = 14590M
 	}
+
+	# TODO(KT): Workaround for https://github.com/analogdevicesinc/linux/issues/3400
+	exec-post = 'gzip -f -k "$IMAGEOUTFILE"'
 }
 
 # ISSI 256Mb OctalRAM and 256 xSPI Flash

board/adi/ev-sc846-som/post-image.sh
@@ -19,6 +19,3 @@ done
 
 # GDB script to load the two U-Boot stages over JTAG
 install -m 0644 "$BOARD_DIR/../u-boot.gdb" "$BINARIES_DIR/u-boot.gdb"
-
-# TODO(KT): Workaround for https://github.com/analogdevicesinc/linux/issues/3400
-gzip -f -k "${BINARIES_DIR}/emmc.img"

configs/adi_sc598_ezkit_defconfig
@@ -6,7 +6,7 @@ BR2_GLOBAL_PATCH_DIR="board/adi/patches"
 BR2_DOWNLOAD_FORCE_CHECK_HASHES=y
 BR2_TARGET_GENERIC_HOSTNAME="sc598-ezkit"
 BR2_TARGET_GENERIC_ISSUE="Welcome to the ADI SC598 EZ-KIT"
-BR2_ROOTFS_POST_IMAGE_SCRIPT="support/scripts/genimage.sh board/adi/ev-sc598-som/post-image.sh"
+BR2_ROOTFS_POST_IMAGE_SCRIPT="board/adi/ev-sc598-som/post-image.sh support/scripts/genimage.sh"
 BR2_ROOTFS_POST_IMAGE_SCRIPT_ARGS="-c board/adi/ev-sc598-som/genimage.cfg"
 BR2_LINUX_KERNEL=y
 BR2_LINUX_KERNEL_CUSTOM_GIT=y

configs/adi_sc598_ezlite_defconfig
@@ -6,7 +6,7 @@ BR2_GLOBAL_PATCH_DIR="board/adi/patches"
 BR2_DOWNLOAD_FORCE_CHECK_HASHES=y
 BR2_TARGET_GENERIC_HOSTNAME="sc598-ezlite"
 BR2_TARGET_GENERIC_ISSUE="Welcome to the ADI SC598 EZ-LITE"
-BR2_ROOTFS_POST_IMAGE_SCRIPT="support/scripts/genimage.sh board/adi/ev-sc598-som/post-image.sh"
+BR2_ROOTFS_POST_IMAGE_SCRIPT="board/adi/ev-sc598-som/post-image.sh support/scripts/genimage.sh"
 BR2_ROOTFS_POST_IMAGE_SCRIPT_ARGS="-c board/adi/ev-sc598-som/genimage.cfg"
 BR2_LINUX_KERNEL=y
 BR2_LINUX_KERNEL_CUSTOM_GIT=y

configs/adi_sc846_ezkit_defconfig
@@ -9,7 +9,7 @@ BR2_DOWNLOAD_FORCE_CHECK_HASHES=y
 BR2_PER_PACKAGE_DIRECTORIES=y
 BR2_TARGET_GENERIC_HOSTNAME="sc846-ezkit"
 BR2_TARGET_GENERIC_ISSUE="Welcome to the ADI SC846 EZ-KIT"
-BR2_ROOTFS_POST_IMAGE_SCRIPT="support/scripts/genimage.sh board/adi/ev-sc846-som/post-image.sh"
+BR2_ROOTFS_POST_IMAGE_SCRIPT="board/adi/ev-sc846-som/post-image.sh support/scripts/genimage.sh"
 BR2_ROOTFS_POST_IMAGE_SCRIPT_ARGS="-c board/adi/ev-sc846-som/genimage.cfg"
 BR2_LINUX_KERNEL=y
 BR2_LINUX_KERNEL_CUSTOM_GIT=y